a91d2665193e6cf53e80f75ff4c5aff6f3214ad6
natanascimento <natanascimentom@outlook.com>
2021-04-12 19:13:25 -0300

    Alterando o nome das variaveis

--- EstruturasDeDados/ListaLigada/ListaLigada.py
+++ EstruturasDeDados/ListaLigada/ListaLigada.py
@@ -15,22 +15,22 @@ class ListaLigada:
     return self._quantidade
   
   def inserir_inicio(self, conteudo):
-    celula = Node(conteudo)
-    celula.proximo = self._inicio
-    self._inicio = celula
+    node = Node(conteudo)
+    node.proximo = self._inicio
+    self._inicio = node
     self._quantidade += 1
     
   def inserir(self, posicao, conteudo):
     if posicao == 0:
       self.inserir_inicio(conteudo)
       return
-    celula = Node(conteudo)
-    esquerda = self._celula(posicao-1)
-    celula.proximo = esquerda.proximo
-    esquerda.proximo = celula
+    node = Node(conteudo)
+    esquerda = self._node(posicao-1)
+    node.proximo = esquerda.proximo
+    esquerda.proximo = node
     self._quantidade += 1
     
-  def _celula(self, posicao):
+  def _node(self, posicao):
     self._validar_posicao(posicao)
     atual = self.inicio
     for i in range(0, posicao):
@@ -45,7 +45,7 @@ class ListaLigada:
   def imprimir(self):
     atual = self.inicio
     for i in range(0, self.quantidade):
-      print(atual.conteudo)
+      print(atual.conteudo) 
       atual = atual.proximo
   
   def remover_inicio(self):
@@ -58,7 +58,7 @@ class ListaLigada:
   def remover(self, posicao):
     if posicao == 0:
       return self.remover_inicio()
-    esquerda = self._celula(posicao - 1)
+    esquerda = self._node(posicao - 1)
     removido = esquerda.proximo
     esquerda.proximo = removido.proximo
     removido.proximo = None
@@ -67,5 +67,5 @@ class ListaLigada:
   
   def item(self, posicao):
     self._validar_posicao(posicao)
-    celula = self._celula(posicao)
-    return celula.conteudo
+    node = self._node(posicao)
+    return node.conteudo
