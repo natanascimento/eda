2ac8be1a89743c7bac16e7be2e11b88c7a614de0
natanascimento <natanascimentom@outlook.com>
2021-04-12 21:17:58 -0300

    Adicionando metodo para fila

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from EstruturasDeDados.ListaLigada.ListaLigada import ListaLigada
 from EstruturasDeDados.ListaDuplamenteLigada.ListaDuplamenteLigada import ListaDuplamenteLigada
 from EstruturasDeDados.Pilha.Pilha import Pilha
+from EstruturasDeDados.Fila.Fila import Fila
 from Loja.Loja import Loja
 from Fase.Fase import Fase
+from Pedido.Pedido import Pedido
 
 
 class EstruturasDeDados:
@@ -24,6 +26,11 @@ class EstruturasDeDados:
     self.fase2 = Fase('Castelo', 400, -200)
     self.fase3 = Fase('Caverna', 100, -30)
     self.fase4 = Fase('Guerra', 3000, -400)
+    # Instanciando pedidos
+    self.pedido1 = Pedido('Pizza de Calabresa')
+    self.pedido2 = Pedido('Pizza de Frango')
+    self.pedido3 = Pedido('Pizza de Muçarela')
+    self.pedido4 = Pedido('Pizza de Rúcula')
     
   def AvaliarLista(self, lista):
     print("\nQuantidade de elementos na lista: {}".format(lista.quantidade))
@@ -32,8 +39,8 @@ class EstruturasDeDados:
     print("")
     
   def ListaLigadaInstance(self):
-    print("##### LISTA LIGADA ######")
-    # Iniciando a lista
+    print("##### LISTA LIGADA #####")
+    # Iniciando a lista ligada
     lista = ListaLigada()
     # Adicionando itens
     lista.inserir_inicio(self.loja1)
@@ -42,44 +49,62 @@ class EstruturasDeDados:
     lista.inserir(1, self.loja4)
     lista.inserir(0, self.loja5)
     lista.inserir(2, self.loja6)
-    
+
+    #Avaliar Lista
     self.AvaliarLista(lista)
+    
     #Remover Itens
     removido = lista.remover_inicio()
     print("Item removido: {}".format(removido))
     removido = lista.remover_inicio()
     print("Item removido: {}".format(removido))
+    
+    #Avaliar Lista
     self.AvaliarLista(lista)
+    
+    #Remover Itens
     removido = lista.remover(1)
     print("Item removido da posicao 1: {}".format(removido))
+    
+    #Avaliar Lista
     self.AvaliarLista(lista)
     removido = lista.remover(lista.quantidade - 1)
     print("Item removido da posicao {}: {}".format(lista.quantidade, removido))
+    
+    #Avaliar Lista
     self.AvaliarLista(lista)
     print("Elemento da posicao {}: {}".format(0, lista.item(0)))
     
   def ListaDuplamenteLigadaInstance(self):
-    print("##### LISTA DUPLAMENTE LIGADA ######")
+    print("##### LISTA DUPLAMENTE LIGADA #####")
+    #Iniciando a lista duplamente ligada
     lista = ListaDuplamenteLigada()
+    #Método para analisar os dados da lista
     self.AvaliarLista(lista)
+    #Inserir conteudo na lista
     lista.inserir_no_inicio(self.loja1)
     lista.inserir_no_inicio(self.loja2)
     lista.inserir_no_fim(self.loja3)
     lista.inserir_no_fim(self.loja4)
+    #Avaliar Lista
     self.AvaliarLista(lista)
     lista.inserir(2, self.loja10)
+    #Avaliar Lista
     self.AvaliarLista(lista)
     
     removido = lista.remover_do_inicio()
     print("Item removido: {}".format(removido))
+    #Avaliar Lista
     self.AvaliarLista(lista)
     
     removido = lista.remover_do_fim()
     print("Item removido: {}".format(removido))
+    #Avaliar Lista
     self.AvaliarLista(lista)
   
     removido = lista.remover(0)
     print("Item removido da posicao 0: {}".format(removido))
+    #Avaliar Lista
     self.AvaliarLista(lista)
     
   def ValidarItensEmRange(self, lista):
@@ -88,6 +113,7 @@ class EstruturasDeDados:
     print("Elementos da Lista no range: {}".format([lista.item(i) for i in range(self._min, self._max)]))
     
   def PilhaInstance(self):
+    print("##### PILHA #####")
     fases = Pilha()
     fases.empilha(self.fase1)
     fases.empilha(self.fase2)
@@ -100,8 +126,34 @@ class EstruturasDeDados:
     print("Falhou na fase {}".format(falhou))
     print("Voltou para a fase {}".format(fases.topo))
     
+  def FilaInstance(self):
+    print("##### FILA #####")
+    pedidos = Fila()
+    #Iniciar pedidos na fila
+    pedidos.entrar(self.pedido1)
+    pedidos.entrar(self.pedido2)
+    pedidos.entrar(self.pedido3)
+    pedidos.entrar(self.pedido4)
+    #Realizar finalização dos pedidos
+    pedido = pedidos.sair()
+    print("Pedido {} finalizado".format(pedido))
+    print("Está vazia? {}".format(pedidos.vazia))
+    
+    pedido = pedidos.sair()
+    print("Pedido {} finalizado".format(pedido))
+    print("Está vazia? {}".format(pedidos.vazia))
+    
+    pedido = pedidos.sair()
+    print("Pedido {} finalizado".format(pedido))
+    print("Está vazia? {}".format(pedidos.vazia))
+
+    pedido = pedidos.sair()
+    print("Pedido {} finalizado".format(pedido))
+    print("Está vazia? {}".format(pedidos.vazia))
+    
 if __name__ == '__main__':
   main = EstruturasDeDados()
-  #main.ListaLigadaInstance()
-  #main.ListaDuplamenteLigadaInstance()
+  main.ListaLigadaInstance()
+  main.ListaDuplamenteLigadaInstance()
   main.PilhaInstance()
+  main.FilaInstance()
